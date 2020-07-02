6a5a0a5e9cc21d91d4dc58c12ac6f8b699a8bb45
Jack Hoover <j.hoover1121@gmail.com>
2020-07-01 19:21:45 -0500

    var changes

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-let userInput
-let uvIndex
-let pastCities = JSON.parse(localStorage.getItem('cities')) || []
-let display = document.getElementById('cityInfo')
-let fiveSelect= document.getElementById('fiveDay')
-let citySelect = document.getElementById('pastCities')
+let input1
+let uvdata
+let loggedcity = JSON.parse(localStorage.getItem('cities')) || []
+let viewpoint = document.getElementById('cityInfo')
+let daysfive = document.getElementById('fiveDay')
+let choosecity = document.getElementById('loggedcity')
 
 //  API KEY : 504fb55759317621b3658208c57633c9
 
 
 //API call by city name: api.openweathermap.org/data/2.5/weather?q=<cityName>
-document.addEventListener('click', ()=>{
+document.addEventListener('click', () => {
   let target = event.target
-  if(target.classList.contains('btn')){
-    userInput = document.getElementById('citySearch').value
-    // console.log(userInput)
-    displayWeather(userInput)
-    //empty out userInput
-    document.getElementById('citySearch').value =''
+  if (target.classList.contains('btn')) {
+    input1 = document.getElementById('citySearch').value
+    // console.log(input1)
+    viewpointWeather(input1)
+    //empty out input1
+    document.getElementById('citySearch').value = ''
   }
 })
 
 
-const renderPastCity = _ =>{
+const renderPastCity = _ => {
   //set to empty before every render
-  citySelect.innerHTML = ''
-  for(let i = 0; i<pastCities.length; i++){
+  choosecity.innerHTML = ''
+  for (let i = 0; i < loggedcity.length; i++) {
     let cityNode = document.createElement('div')
-    cityNode.innerHTML = `${pastCities[i]} <hr>`
-    citySelect.append(cityNode)
+    cityNode.innerHTML = `${loggedcity[i]} <hr>`
+    choosecity.append(cityNode)
   }
 }
-const toFarenheit = value =>{
+const toFarenheit = value => {
   value = (value * (9 / 5) - 459.67).toFixed(2)
   return value
 }
@@ -43,38 +43,38 @@ const titleCase = str => {
   }
   return splitStr.join(' ');
 }
-const displayWeather = userInput =>{
-  //pushing userInput to local storage
-  userInput = titleCase(userInput)
-  pastCities.push(userInput)
-  console.log(pastCities)
-  localStorage.setItem('cities', JSON.stringify(pastCities))
+const viewpointWeather = input1 => {
+  //pushing input1 to local storage
+  input1 = titleCase(input1)
+  loggedcity.push(input1)
+  console.log(loggedcity)
+  localStorage.setItem('cities', JSON.stringify(loggedcity))
   renderPastCity()
-  getCityWeather(userInput)
+  getCityWeather(input1)
 }
 
-const getCityWeather = userInput =>{
-  display.innerHTML = ''
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=504fb55759317621b3658208c57633c9`)
+const getCityWeather = input1 => {
+  viewpoint.innerHTML = ''
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input1}&appid=504fb55759317621b3658208c57633c9`)
     .then(response => response.json())
     .then(({ main: { temp, humidity }, wind: { speed }, coord: { lon, lat } }) => {
       let info = document.createElement('div')
       temp = toFarenheit(temp)
 
       // console.log(temp, humidity, speed, lon, lat)
-      info.innerHTML = `<h2>${userInput} ${moment().format('MM/DD/YYYY')}</h2>
+      info.innerHTML = `<h2>${input1} ${moment().format('MM/DD/YYYY')}</h2>
     <p>Temperature: ${temp} ºF</p>
     <p>Humidity: ${humidity}</p>
     <p> Wind Speed: ${speed} mph </p>
     `
-      display.append(info)
-      getUvIndex(lon, lat)
+      viewpoint.append(info)
+      getuvdata(lon, lat)
       getFiveDayForecast(lon, lat)
-      
+
     })
     .catch(error => console.error(error))
 }
-const getUvIndex = (lon, lat) =>{
+const getuvdata = (lon, lat) => {
   fetch(`https://api.openweathermap.org/data/2.5/uvi?appid=504fb55759317621b3658208c57633c9&lat=${lat}&lon=${lon}`)
     .then(response => response.json())
     .then(({ value }) => {
@@ -83,27 +83,27 @@ const getUvIndex = (lon, lat) =>{
       let uvSpan = document.createElement('span')
       uvSpan.textContent = `${value}`
       value = Math.floor(value)
-      if(value < 3){
+      if (value < 3) {
         uvSpan.setAttribute('class', 'uvSafe')
       }
-      else if (value > 2 && value <6){
+      else if (value > 2 && value < 6) {
         uvSpan.setAttribute('class', 'uvMed')
       }
-      else if (value > 5 && value < 8){
+      else if (value > 5 && value < 8) {
         uvSpan.setAttribute('class', 'uvMod')
       }
-      else{
+      else {
         uvSpan.setAttribute('class', 'uvHigh')
       }
       uvNode.append(uvSpan)
-      display.append(uvNode)
+      viewpoint.append(uvNode)
     })
 
     .catch(error => console.error(error))
 }
 
-const getFiveDayForecast = (lon, lat) =>{
-  fiveSelect.innerHTML =''
+const getFiveDayForecast = (lon, lat) => {
+  daysfive.innerHTML = ''
   fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=504fb55759317621b3658208c57633c9`)
     .then(response => response.json())
     .then(data => {
@@ -111,17 +111,17 @@ const getFiveDayForecast = (lon, lat) =>{
       //converting unix time stamp to a date time
       let list = data.list
       console.log(moment.unix(list[0].dt).format("MM/DD/YYYY"))
-      for(let i = 7; i<list.length; i+=7){
+      for (let i = 7; i < list.length; i += 7) {
         let fiveNode = document.createElement('div')
         fiveNode.setAttribute('class', 'col-sm-2.4 fiveDayStyle')
-        fiveNode.innerHTML =`
+        fiveNode.innerHTML = `
             <h6>${moment.unix(list[i].dt).format("MM/DD/YYYY")}</h6>
             <img src ="https://openweathermap.org/img/wn/${list[i].weather[0].icon}.png" alt = "${list[i].weather[0].icon}">
             <p>Temp: ${toFarenheit(list[i].main.temp)} ºF</p>
             <p>Humidity: ${list[i].main.humidity}%</p>
         `
         console.log(fiveNode)
-        fiveSelect.append(fiveNode)
+        daysfive.append(fiveNode)
       }
       // starts at index 0 and increases by 7
     })
